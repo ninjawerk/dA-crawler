9d3e2c9625c403c849204388038a62990deb2e21
beastsanchez <beastsanchez7@gmail.com>
2017-12-02 20:06:33 +0530

    code cleanup

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"net/url"
-	"runtime"
 	"strings"
 	"sync"
-	"time"
 
 	"github.com/PuerkitoBio/fetchbot"
 	"github.com/PuerkitoBio/goquery"
@@ -41,12 +38,7 @@ var (
 	dup = map[string]bool{}
 	db  *gorm.DB
 	// Command-line flags
-	seed        = flag.String("seed", "https://www.deviantart.com/", "seed URL")
-	cancelAfter = flag.Duration("cancelafter", 0, "automatically cancel the fetchbot after a given time")
-	cancelAtURL = flag.String("cancelat", "", "automatically cancel the fetchbot at a given URL")
-	stopAfter   = flag.Duration("stopafter", 0, "automatically stop the fetchbot after a given time")
-	stopAtURL   = flag.String("stopat", "", "automatically stop the fetchbot at a given URL")
-	memStats    = flag.Duration("memstats", 0, "display memory statistics at a given interval")
+	seed = flag.String("seed", "https://www.deviantart.com/", "seed URL")
 )
 
 func main() {
@@ -82,6 +74,35 @@ func main() {
 		func(ctx *fetchbot.Context, res *http.Response, err error) {
 			// Process the body to find the links
 			doc, err := goquery.NewDocumentFromResponse(res)
+
+			if err != nil {
+				fmt.Printf("[ERR] %s %s - %s\n", ctx.Cmd.Method(), ctx.Cmd.URL(), err)
+				return
+			}
+
+			favstr := doc.Find("div.who-faved-modal").First()
+			favtext := favstr.Parent().First().Text()
+
+			favtextReplaced := strings.Replace(favtext, ",", "", -1)
+			favCount, _ := strconv.Atoi(strings.Split(favtextReplaced, " (who?)")[0])
+
+			imgContainer := doc.Find("div.dev-view-deviation").First()
+			img := imgContainer.Find("img.dev-content-full").First()
+
+			titleContainer := doc.Find("div.dev-title-container ").First()
+			artUrl, _ := titleContainer.Find("a.title").First().Attr("href")
+			title := titleContainer.Find("a.title").First().Text()
+			authorName := titleContainer.Find("a.username").First().Text()
+			authorLink, _ := titleContainer.Find("a.username").First().Attr("href")
+			imageSrc, _ := img.Attr("src")
+			//just double check if its in the db again, sometimes dA directs to Url from aliases
+			var count int
+			db.Table("artworks").Where("Url = ?", artUrl).Count(&count)
+			if count <= 0 {
+				artwork := Artwork{Title: title, Url: artUrl, Artist: authorName, ArtistUrl: authorLink, ImageUrl: imageSrc, FavCount: favCount }
+				db.Create(&artwork)
+			}
+
 			if err != nil {
 				fmt.Printf("[ERR] %s %s - %s\n", ctx.Cmd.Method(), ctx.Cmd.URL(), err)
 				return
@@ -91,88 +112,13 @@ func main() {
 			enqueueLinks(ctx, doc)
 		}))
 
-	// Handle HEAD requests for html responses coming from the source host - we don't want
-	// to crawl links from other hosts.
-	mux.Response().Method("GET").ContentType("text/html").Handler(fetchbot.HandlerFunc(
-		func(ctx *fetchbot.Context, res *http.Response, err error) {
-			if _, err := ctx.Q.SendStringGet(ctx.Cmd.URL().String()); err != nil {
-				fmt.Printf("[ERR] %s %s - %s\n", ctx.Cmd.Method(), ctx.Cmd.URL(), err)
-			}
-
-			doc, err := goquery.NewDocumentFromResponse(res)
-			if err != nil {
-				fmt.Printf("[ERR] %s %s - %s\n", ctx.Cmd.Method(), ctx.Cmd.URL(), err)
-				return
-			}
-			favstr := doc.Find("div.who-faved-modal").First()
-			favtext :=  favstr.Parent().First().Text()
-
-			favtextReplaced := strings.Replace(favtext,",","",-1)
-			favCount, _ := strconv.Atoi(strings.Split(favtextReplaced, " (who?)")[0])
-
-			imgContainer := doc.Find("div.dev-view-deviation").First()
-			img := imgContainer.Find("img.dev-content-full").First()
-
-
-				titleContainer := doc.Find("div.dev-title-container ").First()
-				artUrl, _ := titleContainer.Find("a.title").First().Attr("href")
-				title := titleContainer.Find("a.title").First().Text()
-				authorName := titleContainer.Find("a.username").First().Text()
-				authorLink, _ := titleContainer.Find("a.username").First().Attr("href")
-				imageSrc, _ := img.Attr("src")
-				//just double check if its in the db again, sometimes dA directs to Url from aliases
-				var count int
-				db.Table("artworks").Where("Url = ?", artUrl).Count(&count)
-				if count <= 0 {
-					artwork := Artwork{Title: title, Url: artUrl, Artist: authorName, ArtistUrl: authorLink, ImageUrl: imageSrc, FavCount:favCount }
-					db.Create(&artwork)
-				}
-
-		}))
-
 	// Create the Fetcher, handle the logging first, then dispatch to the Muxer
 	h := logHandler(mux)
-	if *stopAtURL != "" || *cancelAtURL != "" {
-		stopURL := *stopAtURL
-		if *cancelAtURL != "" {
-			stopURL = *cancelAtURL
-		}
-		h = stopHandler(stopURL, *cancelAtURL != "", logHandler(mux))
-	}
-	f := fetchbot.New(h)
 
-	// First mem stat print must be right after creating the fetchbot
-	if *memStats > 0 {
-		// Print starting stats
-		printMemStats(nil)
-		// Run at regular intervals
-		runMemStats(f, *memStats)
-		// On exit, print ending stats after a GC
-		defer func() {
-			runtime.GC()
-			printMemStats(nil)
-		}()
-	}
+	f := fetchbot.New(h)
 
 	// Start processing
 	q := f.Start()
-
-	// if a stop or cancel is requested after some duration, launch the goroutine
-	// that will stop or cancel.
-	if *stopAfter > 0 || *cancelAfter > 0 {
-		after := *stopAfter
-		stopFunc := q.Close
-		if *cancelAfter != 0 {
-			after = *cancelAfter
-			stopFunc = q.Cancel
-		}
-
-		go func() {
-			c := time.After(after)
-			<-c
-			stopFunc()
-		}()
-	}
 
 	// Enqueue the seed, which is the first entry in the dup map
 	dup[*seed] = true
@@ -181,67 +127,6 @@ func main() {
 		fmt.Printf("[ERR] GET %s - %s\n", *seed, err)
 	}
 	q.Block()
-}
-
-func runMemStats(f *fetchbot.Fetcher, tick time.Duration) {
-	var mu sync.Mutex
-	var di *fetchbot.DebugInfo
-
-	// Start goroutine to collect fetchbot debug info
-	go func() {
-		for v := range f.Debug() {
-			mu.Lock()
-			di = v
-			mu.Unlock()
-		}
-	}()
-	// Start ticker goroutine to print mem stats at regular intervals
-	go func() {
-		c := time.Tick(tick)
-		for range c {
-			mu.Lock()
-			printMemStats(di)
-			mu.Unlock()
-		}
-	}()
-}
-
-func printMemStats(di *fetchbot.DebugInfo) {
-	var mem runtime.MemStats
-	runtime.ReadMemStats(&mem)
-	buf := bytes.NewBuffer(nil)
-	buf.WriteString(strings.Repeat("=", 72) + "\n")
-	buf.WriteString("Memory Profile:\n")
-	buf.WriteString(fmt.Sprintf("\tAlloc: %d Kb\n", mem.Alloc/1024))
-	buf.WriteString(fmt.Sprintf("\tTotalAlloc: %d Kb\n", mem.TotalAlloc/1024))
-	buf.WriteString(fmt.Sprintf("\tNumGC: %d\n", mem.NumGC))
-	buf.WriteString(fmt.Sprintf("\tGoroutines: %d\n", runtime.NumGoroutine()))
-	if di != nil {
-		buf.WriteString(fmt.Sprintf("\tNumHosts: %d\n", di.NumHosts))
-	}
-	buf.WriteString(strings.Repeat("=", 72))
-	fmt.Println(buf.String())
-}
-
-// stopHandler stops the fetcher if the stopurl is reached. Otherwise it dispatches
-// the call to the wrapped Handler.
-func stopHandler(stopurl string, cancel bool, wrapped fetchbot.Handler) fetchbot.Handler {
-	return fetchbot.HandlerFunc(func(ctx *fetchbot.Context, res *http.Response, err error) {
-		if ctx.Cmd.URL().String() == stopurl {
-			fmt.Printf(">>>>> STOP URL %s\n", ctx.Cmd.URL())
-			// generally not a good idea to stop/block from a handler goroutine
-			// so do it in a separate goroutine
-			go func() {
-				if cancel {
-					ctx.Q.Cancel()
-				} else {
-					ctx.Q.Close()
-				}
-			}()
-			return
-		}
-		wrapped.Handle(ctx, res, err)
-	})
 }
 
 // logHandler prints the fetch information and dispatches the call to the wrapped Handler.
@@ -280,6 +165,8 @@ func enqueueLinks(ctx *fetchbot.Context, doc *goquery.Document) {
 				db.Create(&nlink)
 			}
 		}
+
+		doc = nil
 	})
 	mu.Unlock()
 }
